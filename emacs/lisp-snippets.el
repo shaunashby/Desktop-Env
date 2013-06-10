89fc5a7a8abfb7e167752c1d0e4f23a460a45ae7
Shaun Ashby <Shaun.Ashby@gmail.com>
2013-06-10 16:57:05 +0200

    Clean out elisp scratchpad files.

--- emacs/lisp-snippets.el
+++
@@ -1,9 +0,0 @@
-;; Ruby mode:
-(autoload 'ruby-mode "ruby-mode" "Load ruby-mode")
-(add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
-
-;; Turn on syntax highlighting
-(add-hook 'ruby-mode-hook 'turn-on-font-lock)
-
-;; To make scripts executable automatically:
-(add-hook 'after-save-hook 'executable-make-buffer-file-executable-if-script-p)
