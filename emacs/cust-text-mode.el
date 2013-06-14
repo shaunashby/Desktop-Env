64fdbbac4dcb4018fba0021d176214dfeaf7cf39
Shaun Ashby <Shaun.Ashby@gmail.com>
2013-06-14 15:27:07 +0200

    Change hook method for font-lock and text-fill behaviours in text-mode.

--- emacs/cust-text-mode.el
+++ emacs/cust-text-mode.el
@@ -4,15 +4,14 @@
 
 ;; The hook:
 (add-hook 'text-mode-hook 'turn-on-font-lock)
+(add-hook 'text-mode-hook 'turn-on-auto-fill)
 
 (add-hook 'text-mode-hook
 	  (function (lambda ()
 		      (define-key text-mode-map "\C-xc" 'xcode-template-macroize)
-		      (font-lock-mode t)
-		      (auto-fill-mode t)
 		      )))
 
-(setq auto-mode-alist (cons '("\\.txt\\'" . text-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.txt|\\.letter\\'" . text-mode) auto-mode-alist))
 
 ;;
 ;; End of cust-text-mode.el
