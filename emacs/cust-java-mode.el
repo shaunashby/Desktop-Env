ee11d104986f708e153630515ed179bc14f73225
Shaun Ashby <Shaun.Ashby@gmail.com>
2013-06-17 23:29:49 +0200

    Remove Java insert-file-header function - moved to auto-insert customizations.

--- emacs/cust-java-mode.el
+++ emacs/cust-java-mode.el
@@ -12,39 +12,11 @@
        ))
 
 (font-lock-add-keywords 'java-mode cust-java-font-lock-keywords)
-
-;; Function to generate header:
-(defun java-insert-file-header () 
-  (interactive)
-  "Inserts some lines for a header, including VCS info, author, date and copyright."
-  (insert 
-   "//____________________________________________________________________ 
-// File: " (buffer-name) "
-//____________________________________________________________________ 
-//  
-// Author: " (user-full-name)  " <" user-mail-address ">
-// Update: " (format-time-string "%Y-%m-%d %T%z") "
-// Revision: $Id" "$ 
-//
-// Copyright: " (format-time-string "%Y") " (C) " (user-full-name) "
-//
-//--------------------------------------------------------------------
-"))
-
 ;;
 (add-hook 'java-mode-hook
 	  (function (lambda ()
-		      (require 'easymenu)
-		      (easy-menu-define java-menu java-mode-map "Java Menu"
-			'("Insert"
-			  ["Insert File Header" java-insert-file-header t]
-			  ))
 		      ;;
 		      (define-key java-mode-map "\C-m" 'newline-and-indent))))
-;;
-(setq auto-mode-alist (append '(("\\.java\\'" . java-mode))
-			      auto-mode-alist))
-
 ;;
 ;; End of cust-java-mode.el
 ;;
