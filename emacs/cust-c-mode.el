cb9415487df16bb7e6fc0b2bb63c0a5c92a91e98
Shaun Ashby <Shaun.Ashby@gmail.com>
2013-06-18 17:28:33 +0200

    Re-add couple of functions to C-mode customizations.

--- emacs/cust-c-mode.el
+++ emacs/cust-c-mode.el
@@ -2,6 +2,26 @@
 ;; cust-c-mode.el
 ;;
 
+;; Function to insert a system header:
+(defun system-include-header (&optional header)
+  "Insert an #include for a system header"
+  (interactive)
+  (while (or (not header) (string= header ""))
+    (setq header (read-string "Header name: ")))
+  (insert "#include <" (symbol-value 'header) ">\n"))
+
+;; function to insert a local header:
+(defun local-include-header (&optional header)
+  "insert an #include for a local header file"
+  (interactive)
+  (while (or (not header) (string= header ""))
+    (setq header (read-string "header name (minus the .h): ")))
+  (insert  "#ifndef " (upcase header) "_H\n"
+	   "#define " (upcase header) "_H\n"
+	   "#include \"" (symbol-value 'header) ".h\"\n"
+	   "#endif\n"
+	   ))
+
 ;; To insert a new compile command:
 (defun insert-c-compile-command (&optional compiler)
   (interactive)
