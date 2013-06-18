b476db0e150acbcefd8e80d744a4101c900e36fa
Shaun Ashby <Shaun.Ashby@gmail.com>
2013-06-18 17:22:37 +0200

    Insert license and copyright blocks.

--- emacs/cust-autoinsert.el
+++ emacs/cust-autoinsert.el
@@ -32,9 +32,9 @@
 // Author: " (user-full-name)  " <" user-mail-address ">
 // Update: " (format-time-string "%Y-%m-%d %T%z") "
 // Revision: $Id" "$
+// Description: " (read-string "Description: ") "
 //
-// Copyright: " (format-time-string "%Y") " (C) " (user-full-name) "
-//
+// Copyright (C) " (format-time-string "%Y") " " (user-full-name) "
 //
 //------------------------------------------------------------------------
 
@@ -82,8 +82,7 @@
 // Update: " (format-time-string "%Y-%m-%d %T%z") "
 // Revision: $Id" "$
 //
-// Copyright: " (format-time-string "%Y") " (C) " (user-full-name) "
-//
+// Copyright (C) " (format-time-string "%Y") " " (user-full-name) "
 //
 //------------------------------------------------------------------------
 
@@ -123,26 +122,85 @@ private:
 // Author: " (user-full-name)  " <" user-mail-address ">
 // Update: " (format-time-string "%Y-%m-%d %T%z") "
 // Revision: $Id" "$
+// Description: " (read-string "Description: ") "
 //
-// Copyright: " (format-time-string "%Y") " (C) " (user-full-name) "
+// Copyright (C) " (format-time-string "%Y") " " (user-full-name) "
 //
 //--------------------------------------------------------------------
 ")
-    ;; C:
-    (("\\.c\\'" . "C Source File")
+
+    ;; C main:
+    (("main.c\\'" . "C Source File for Executables")
      nil
-"/*********************************************************************
+"/*****************************************************************************************
  File: " (buffer-name) "
-----------------------------------------------------------------------
+-------------------------------------------------------------------------------------------
 
  Author: " (user-full-name)  " <" user-mail-address ">
  Update: " (format-time-string "%Y-%m-%d %T%z") "
  Revision: $Id" "$
+ Description: " (read-string "Description: ") "
 
- Copyright: " (format-time-string "%Y") " (C) " (user-full-name) "
+ Copyright (C) " (format-time-string "%Y") " " (user-full-name) "
 
-*********************************************************************/
-")) auto-insert-alist))
+ Licensed under the Apache License, Version 2.0 (the \"License\");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an \"AS IS\" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+*******************************************************************************************/
+#include <stdlib.h>
+#include <stdio.h>
+
+/* main */
+int main(int argc, char *argv[]) {
+  " - "
+}")
+    ;; Other C files:
+    (("\\.c\\'" . "C Source File")
+     nil
+     '(setq project (read-string "Project name: "))
+"/*****************************************************************************************
+ File: " (buffer-name) "
+-------------------------------------------------------------------------------------------
+
+ Author: " (user-full-name)  " <" user-mail-address ">
+ Update: " (format-time-string "%Y-%m-%d %T%z") "
+ Revision: $Id" "$
+ Description: " (read-string "Description: ") "
+
+ Copyright (C) " (format-time-string "%Y") " " (user-full-name) "
+
+ This file is part of " project ".
+
+ " project " is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ " project " is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with " project ". If not, see <http://www.gnu.org/licenses/>.
+
+*******************************************************************************************/
+")
+    ;; License files:
+    (("LICENSE.txt" . "License file for GNU-like packages")
+     nil
+     "
+")
+) auto-insert-alist))
 
 ;; Automatic insertion of content from templates:
-;;(define-auto-insert             "\.rb" "hello.rb")
+(define-auto-insert             "COPYING" "License/GPLv3.txt")
