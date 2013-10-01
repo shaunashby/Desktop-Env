e1dfdbb6eaf574bd7445d571ee05d09931fc8583
Shaun Ashby <Shaun.Ashby@gmail.com>
2013-10-01 16:30:32 +0200

    Add auto-insert support for JavaScript and AppleScript files.

--- emacs/cust-autoinsert.el
+++ emacs/cust-autoinsert.el
@@ -408,6 +408,82 @@ use strict;
 ") "
 " - "
 ")
+;; JavaScript:
+(("\\.\\(js\\|JS\\)\\'" . "JavaScript Source File")
+ nil
+"//____________________________________________________________________
+// File: " (buffer-name) "
+//____________________________________________________________________
+//
+// Author: " (user-full-name)  " <" user-mail-address ">
+// Created: " (format-time-string "%Y-%m-%d %T%z") " (Time-stamp: <>)
+// Revision: $Id" "$
+// Description: " (read-string "Description: ") "
+//
+// Copyright (C) " (format-time-string "%Y") " " (user-full-name) "
+//
+//
+//--------------------------------------------------------------------
+
+var f_ = function() {
+" - "
+};
+fn_.call(this);
+")
+;; Go:
+(("\\.\\(go\\|golang\\)\\'" . "Golang Source File")
+ nil
+ '(setq package (file-name-sans-extension (buffer-name)))
+"//____________________________________________________________________
+// File: " (buffer-name) "
+//____________________________________________________________________
+//
+// Author: " (user-full-name)  " <" user-mail-address ">
+// Created: " (format-time-string "%Y-%m-%d %T%z") " (Time-stamp: <>)
+// Revision: $Id" "$
+// Description: " (read-string "Description: ") "
+//
+// Copyright (C) " (format-time-string "%Y") " " (user-full-name) "
+//
+//
+//--------------------------------------------------------------------
+package " package "
+
+import (
+       \"fmt\"
+       \"io\"
+)
+" - "
+")
+;; AppleScript:
+(("\\.\\(applescript\\|scpt\\)\\'" . "AppleScript Source File")
+ nil
+"---
+--- File: " (buffer-name) "
+---
+--- Author: " (user-full-name)  " <" user-mail-address ">
+--- Created: " (format-time-string "%Y-%m-%d %T%z") "
+--- Revision: $Id" "$
+--- Description: " (read-string "Description: ") "
+---
+--- Copyright (C) " (format-time-string "%Y") " " (user-full-name) "
+---
+--- Licensed under the Apache License, Version 2.0 (the \"License\");
+--- you may not use this file except in compliance with the License.
+--- You may obtain a copy of the License at
+---
+---    http://www.apache.org/licenses/LICENSE-2.0
+---
+--- Unless required by applicable law or agreed to in writing, software
+--- distributed under the License is distributed on an \"AS IS\" BASIS,
+--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--- See the License for the specific language governing permissions and
+--- limitations under the License.
+---
+-----------------------------------------------------------------------
+" - "
+")
+
 ) auto-insert-alist))
 
 ;; Automatic insertion of content from templates:
