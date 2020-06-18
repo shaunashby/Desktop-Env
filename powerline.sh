078f6a12726c87c434bb9cd61e480ee93c74ff25
Shaun Ashby <shaun.ashby@gmail.com>
2020-06-18 17:14:46 +0200

    Fix Powerline font installer script

--- powerline.sh
+++ powerline.sh
@@ -8,13 +8,16 @@ fi
 echo "Installing Powerline fonts..."
 
 if [[ ! -d $HOME/fonts ]]; then
+	cd $HOME
 	git clone https://github.com/powerline/fonts.git --depth=1
 	# Run the installer:
-	cd fonts; ./install.sh
+	cd fonts; ./install.sh; cd ..
+	# Clean up:
+	rm -rf $HOME/fonts
 	# Touch a file to say we are done:
 	touch .env/powerline
 else
-	echo "Powerline fonts are already downloaded. Did you run the installer?
+	echo "Powerline fonts are already downloaded. Did you run the installer?"
 fi
 
 exit 0
